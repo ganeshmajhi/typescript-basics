faab64f8af90857058d3dfa69d6f27b590824a81
ganeshmajhi <ganeshmajhi@gmail.com>
2022-01-30 00:28:33 +0530

    added src

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,48 @@
-let id: number = 5; 
+// Basic Types
+let id: number = 5
+let company: string = "21n78e Creative Labs"
+let isPublished: boolean = true
+let x:any = "Hello"
 
-console.log('ID:', id);
+// Array
+
+let ids: number[] = [1,2,3,4,5]
+let arrs: any[] = [1,3,'doit', true]
+
+//Tuple
+
+let person:[number,string,boolean] = [1,'ganesh',true]
+
+//Tuple Array
+
+let emplyoee:[number, string][]
+
+emplyoee=[
+    [1,'gani'],
+    [2,'somi'],
+    [3,'romi']
+]
+
+//Union
+
+let pid: string | number;
+
+pid=23
+
+//Enum
+
+enum direction1{
+    Up =1,
+    Down,
+    Left,
+    Right
+}
+console.log(direction1.Left)
+
+enum direction2{
+    Up = 'so',
+    Down = 'much',
+    Left ='love',
+    Right ='for'
+}
+console.log(direction2.Left)
